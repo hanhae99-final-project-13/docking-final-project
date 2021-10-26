f9f66d6ffeeb39cfc2e0b0d38f0625cc1c00d701
zzangoobrother <com1368@naver.com>
2021-10-26 22:17:23 +0900

    [feat] project setting

---
+++ src/main/java/com/sparta/dockingfinalproject/DockingFinalProjectApplication.java
@@ -0,0 +1,13 @@
+package com.sparta.dockingfinalproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DockingFinalProjectApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DockingFinalProjectApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/sparta/dockingfinalproject/DockingFinalProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sparta.dockingfinalproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DockingFinalProjectApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
