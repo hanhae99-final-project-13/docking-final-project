6cc98122adb05b1554afd6736f029c64acf4302f
Jieun Song <80088918+wldms2494@users.noreply.github.com>
2021-10-26 23:58:57 +0900

    [feat] User 테이블 추가

--- src/main/java/com/sparta/dockingfinalproject/user/User.java
+++ src/main/java/com/sparta/dockingfinalproject/user/User.java
@@ -1,5 +1,26 @@
 package com.sparta.dockingfinalproject.user;
 
+import javax.persistence.*;
+
+@Entity
 public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int userId;
+    @Column(nullable = false)
+    private String username;
+
+    @Column(nullable = false)
+    private String password;
+
+    @Column(nullable = false)
+    private String nickname;
+
+    @Column(nullable = false)
+    private String email;
+
+    @Column(nullable = false)
+    private String badge;
+
 
 }
