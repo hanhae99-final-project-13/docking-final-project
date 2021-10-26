9e75f79479937a659e92e85643ce6a9c5456d171
zzangoobrother <com1368@naver.com>
2021-10-27 02:24:03 +0900

    [feat] 공통사용 Timestamped 기능 구현

--- src/main/java/com/sparta/dockingfinalproject/post/Post.java
+++ src/main/java/com/sparta/dockingfinalproject/post/Post.java
@@ -1,5 +1,12 @@
 package com.sparta.dockingfinalproject.post;
 
+import javax.persistence.Entity;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Getter
+@NoArgsConstructor
 public class Post {
 
 }
