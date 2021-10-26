b6666dcd4296dce2a71335ccc988196ff3eeec5a
zzangoobrother <com1368@naver.com>
2021-10-27 02:49:51 +0900

    [feat] pet model 변수들 구현

--- src/main/java/com/sparta/dockingfinalproject/pet/Pet.java
+++ src/main/java/com/sparta/dockingfinalproject/pet/Pet.java
@@ -1,5 +1,61 @@
 package com.sparta.dockingfinalproject.pet;
 
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Getter
+@NoArgsConstructor
 public class Pet {
 
+  @Id
+  @GeneratedValue(strategy = GenerationType.AUTO)
+  private Long petId;
+
+  @Column(nullable = false)
+  private String petName;
+
+  @Column(nullable = false)
+  private String breed;
+
+  @Column(nullable = false)
+  private String sex;
+
+  @Column(nullable = false)
+  private int age;
+
+  @Column(nullable = false)
+  private double weight;
+
+  @Column(nullable = false)
+  private String lostLocation;
+
+  @Column(nullable = false)
+  private String ownerType;
+
+  @Column(nullable = false)
+  private String address;
+
+  @Column(nullable = false)
+  private String phone;
+
+  @Column(nullable = false)
+  private String tag;
+
+  @Column(nullable = false)
+  private String url;
+
+  @Column(nullable = false)
+  private String img;
+
+  @Column(nullable = false)
+  private String extra;
+
+  @Column(nullable = false)
+  private String isAdopted;
 }
