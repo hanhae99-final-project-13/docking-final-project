9c227ac53e5203a468f84a48f9ac657d4680d83b
zzangoobrother <com1368@naver.com>
2021-10-27 13:11:04 +0900

    [feat] PostRepository 생성

---
+++ src/main/java/com/sparta/dockingfinalproject/post/PostRepository.java
@@ -0,0 +1,7 @@
+package com.sparta.dockingfinalproject.post;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface PostRepository extends JpaRepository<Post, Long> {
+  
+}
