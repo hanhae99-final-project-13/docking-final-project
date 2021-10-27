9f92b24d61d156bb6a44ce0fdbe8f3c996566f0a
zzangoobrother <com1368@naver.com>
2021-10-28 01:02:00 +0900

    [feat] PetRepository 생성

---
+++ src/main/java/com/sparta/dockingfinalproject/pet/PetRepository.java
@@ -0,0 +1,7 @@
+package com.sparta.dockingfinalproject.pet;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface PetRepository extends JpaRepository<Pet, Long> {
+
+}
