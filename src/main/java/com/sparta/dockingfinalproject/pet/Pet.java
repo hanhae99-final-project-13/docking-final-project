2d6c524b261baf4e8b92fc97c4005d8b7fec8730
zzangoobrother <com1368@naver.com>
2021-10-28 16:37:44 +0900

    [feat] pet update 기능 추가

--- src/main/java/com/sparta/dockingfinalproject/pet/Pet.java
+++ src/main/java/com/sparta/dockingfinalproject/pet/Pet.java
@@ -84,4 +84,54 @@ public class Pet {
   public void addPost(Post post) {
     this.post = post;
   }
+
+  public Pet update(PetRequestDto petRequestDto) {
+    if (petCheck(petRequestDto.getBreed())) {
+      this.breed = petRequestDto.getBreed();
+    }
+    this.age = petRequestDto.getAge();
+    this.weight = petRequestDto.getWeight();
+
+    if (petCheck(petRequestDto.getLostLocation())) {
+      this.lostLocation= petRequestDto.getLostLocation();
+    }
+
+    if (petCheck(petRequestDto.getOwnerType())) {
+      this.ownerType= petRequestDto.getOwnerType();
+    }
+
+    if (petCheck(petRequestDto.getAddress())) {
+      this.address= petRequestDto.getAddress();
+    }
+
+    if (petCheck(petRequestDto.getPhone())) {
+      this.phone= petRequestDto.getPhone();
+    }
+
+    if (petCheck(petRequestDto.getTag())) {
+      this.tag= petRequestDto.getTag();
+    }
+
+    if (petCheck(petRequestDto.getUrl())) {
+      this.url= petRequestDto.getUrl();
+    }
+
+    if (petCheck(petRequestDto.getImg())) {
+      this.img= petRequestDto.getImg();
+    }
+
+    if (petCheck(petRequestDto.getExtra())) {
+      this.extra= petRequestDto.getExtra();
+    }
+
+    if (petCheck(petRequestDto.getIsAdopted())) {
+      this.isAdopted= petRequestDto.getIsAdopted();
+    }
+
+    return this;
+  }
+
+  private boolean petCheck(String data) {
+    return data != null && !data.isEmpty();
+  }
 }
