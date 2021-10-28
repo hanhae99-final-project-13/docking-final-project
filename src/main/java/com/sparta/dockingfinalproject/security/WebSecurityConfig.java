89d25227ba909d88ea0dfd185ec32e7be453805b
vktm91@nate.com <vktm91@nate.com>
2021-10-28 23:32:11 +0900

    [feat] api 기능 추가  + Pet Class petName컬럼 삭제/ tag, url (nullable = false) 삭제/ pet생성자(api받는용도)추가/ 컬럼순서변경

--- src/main/java/com/sparta/dockingfinalproject/security/WebSecurityConfig.java
+++ src/main/java/com/sparta/dockingfinalproject/security/WebSecurityConfig.java
@@ -27,11 +27,6 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
         this.jwtTokenProvider = jwtTokenProvider;
     }
 
-    @Override
-    public void configure(WebSecurity web) throws Exception {
-        web.ignoring().antMatchers("/h2-console/**");
-    }
-
     @Bean
     public BCryptPasswordEncoder encodePassword(){
         return new BCryptPasswordEncoder();
