71d6a1746221f858a733b39880574b653497f23a
zzangoobrother <com1368@naver.com>
2021-10-30 22:57:46 +0900

    [fix] url 수정 postId 수정

--- src/main/java/com/sparta/dockingfinalproject/wish/WishController.java
+++ src/main/java/com/sparta/dockingfinalproject/wish/WishController.java
@@ -1,10 +1,11 @@
 package com.sparta.dockingfinalproject.wish;
 
 import com.sparta.dockingfinalproject.security.UserDetailsImpl;
+import com.sparta.dockingfinalproject.wish.dto.WishRequestDto;
 import java.util.Map;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
-import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -16,8 +17,8 @@ public class WishController {
     this.wishService = wishService;
   }
 
-  @PostMapping("/wishes/{postId}")
-  public Map<String, Object> addWish(@PathVariable Long postId, @AuthenticationPrincipal UserDetailsImpl userDetails) {
-    return wishService.addWish(postId, userDetails);
+  @PostMapping("/wishes")
+  public Map<String, Object> addWish(@RequestBody WishRequestDto wishRequestDto,@AuthenticationPrincipal UserDetailsImpl userDetails) {
+    return wishService.addWish(wishRequestDto.getPostId(), userDetails);
   }
 }
---
+++ src/main/java/com/sparta/dockingfinalproject/wish/dto/WishRequestDto.java
@@ -0,0 +1,13 @@
+package com.sparta.dockingfinalproject.wish.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor
+public class WishRequestDto {
+
+  private Long postId;
+}
