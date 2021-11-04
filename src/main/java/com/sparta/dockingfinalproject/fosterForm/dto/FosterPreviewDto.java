c328bd8f325e258f38900db0625436720c4af050
vktm91@nate.com <vktm91@nate.com>
2021-11-04 15:28:57 +0900

    [feat] 마이페이지 입양신청서 조회 기능 추가

---
+++ src/main/java/com/sparta/dockingfinalproject/fosterForm/dto/FosterPreviewDto.java
@@ -0,0 +1,33 @@
+package com.sparta.dockingfinalproject.fosterForm.dto;
+
+
+import com.sparta.dockingfinalproject.fosterForm.FosterForm;
+import com.sparta.dockingfinalproject.pet.Sex;
+import lombok.Builder;
+import lombok.Getter;
+
+@Getter
+@Builder
+public class FosterPreviewDto {
+
+  private Long fosterFormId;
+  private String name;
+  private Long birthYear;
+  private Sex gender;
+  private String phone;
+  private String fosterAddress;
+  private PostPreviewDto postPreview;
+
+  public static FosterPreviewDto of(FosterForm fosterForm, PostPreviewDto postPreviewDto) {
+    return FosterPreviewDto.builder()
+        .fosterFormId(fosterForm.getFosterFormId())
+        .name(fosterForm.getName())
+        .birthYear(fosterForm.getBirthYear())
+        .gender(fosterForm.getGender())
+        .phone(fosterForm.getPhone())
+        .fosterAddress(fosterForm.getFosterAddress())
+        .postPreview(postPreviewDto)
+        .build();
+  }
+
+}
