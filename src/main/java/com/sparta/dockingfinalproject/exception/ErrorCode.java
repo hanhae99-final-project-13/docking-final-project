f855259ea156324ac14d6bc987632b00c753c93a
vktm91@nate.com <vktm91@nate.com>
2021-11-04 15:28:01 +0900

    [feat] 마이페이지 입양신청서 조회 기능 추가

--- src/main/java/com/sparta/dockingfinalproject/exception/ErrorCode.java
+++ src/main/java/com/sparta/dockingfinalproject/exception/ErrorCode.java
@@ -14,15 +14,14 @@ public enum ErrorCode {
   USER_NOT_FOUND(BAD_REQUEST, "회원 정보를 찾을 수 없습니다."),
   LOGIN_TOKEN_EXPIRE(BAD_REQUEST, "로그인이 만료되었습니다. 재로그인 하세요."),
   POST_NOT_FOUND(BAD_REQUEST, "해당 게시글을 찾을 수 없습니다."),
+  FOSTERFORM_NOT_FOUND(BAD_REQUEST, "해당 입양신청서를 찾을 수 없습니다."),
   ALARM_NOT_FOUND(BAD_REQUEST, "해당 알람 내용을 찾을 수 없습니다."),
   NO_AUTHORIZATION(BAD_REQUEST, "권한이 없습니다."),
   NO_DIFFERENCE(BAD_REQUEST, "변경된 사항이 없습니다."),
-  PASSWORD_MISS_MATCH(BAD_REQUEST, "비밀번호가 일치하지 않습니다"),
-  USERNAME_MISS_MATCH(BAD_REQUEST, "아이디가 일치하지 않습니다"),
+  PASSWORD_MISS_MATCH(BAD_REQUEST, "비밀번호가 일치하지 않습니다."),
+  USERNAME_MISS_MATCH(BAD_REQUEST, "아이디가 일치하지 않습니다."),
   USERNAME_DUPLICATE(BAD_REQUEST, "중복된 username이 존재합니다."),
   NICKNAME_DUPLICATE(BAD_REQUEST, "중복된 nickname이 존재합니다."),
-
-
   PET_NOT_FOUND(BAD_REQUEST, "해당 유기견을 찾을 수 없습니다.");
 
   private final HttpStatus httpStatus;
--- src/main/java/com/sparta/dockingfinalproject/fosterForm/FosterFormService.java
+++ src/main/java/com/sparta/dockingfinalproject/fosterForm/FosterFormService.java
@@ -4,12 +4,16 @@ import com.sparta.dockingfinalproject.common.SuccessResult;
 import com.sparta.dockingfinalproject.exception.DockingException;
 import com.sparta.dockingfinalproject.exception.ErrorCode;
 import com.sparta.dockingfinalproject.fosterForm.dto.FosterFormRequestDto;
+import com.sparta.dockingfinalproject.fosterForm.dto.FosterFormResultDto;
+import com.sparta.dockingfinalproject.fosterForm.dto.FosterPreviewDto;
 import com.sparta.dockingfinalproject.post.Post;
 import com.sparta.dockingfinalproject.post.PostRepository;
 import com.sparta.dockingfinalproject.security.UserDetailsImpl;
 import com.sparta.dockingfinalproject.user.User;
 import com.sparta.dockingfinalproject.user.UserRepository;
+import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 import javax.transaction.Transactional;
 import lombok.RequiredArgsConstructor;
@@ -27,6 +31,10 @@ public class FosterFormService {
   @Transactional
   public Map<String, Object> addFosterForm(Long postId, FosterFormRequestDto fosterFormrRequestDto,
       UserDetailsImpl userDetails) {
+    if (userDetails == null) {
+      throw new DockingException(ErrorCode.USER_NOT_FOUND);
+    }
+
     //User 가져오기
     Long userId = userDetails.getUser().getUserId();
     User user = userRepository.getById(userId);
@@ -44,7 +52,61 @@ public class FosterFormService {
     Map<String, Object> data = new HashMap<>();
     data.put("msg", "입양 신청이 완료 되었습니다");
     return SuccessResult.success(data);
-
   }
 
+  //입양 신청서 상세조회
+  @Transactional
+  public Map<String, Object> getFosterForm(Long fosterFormId, UserDetailsImpl userDetails) {
+    if (userDetails == null) {
+      throw new DockingException(ErrorCode.USER_NOT_FOUND);
+    }
+    Map<String, Object> data = new HashMap<>();
+
+    Long userId = userDetails.getUser().getUserId();
+    User user = userRepository.getById(userId);
+
+    FosterForm findFosterForm = fosterFormRepository.findById(fosterFormId).orElseThrow(
+        () -> new DockingException(ErrorCode.FOSTERFORM_NOT_FOUND)
+    );
+
+    Long postWriterId = findFosterForm.getPost().getUser().getUserId();
+    Long writerId = findFosterForm.getUser().getUserId();
+    if (userId.equals(writerId) || userId.equals(postWriterId)) {
+      FosterFormResultDto fosterFormResultDto = FosterFormResultDto.of(findFosterForm);
+
+      data.put("FosterForm", fosterFormResultDto);
+    } else {
+      throw new DockingException(ErrorCode.NO_AUTHORIZATION);
+    }
+    return SuccessResult.success(data);
+  }
+
+  //내가 보낸 입양신청서 목록조회
+  @Transactional
+  public Map<String, Object> getMyFosterForms(UserDetailsImpl userDetails) {
+    if (userDetails == null) {
+      throw new DockingException(ErrorCode.USER_NOT_FOUND);
+    }
+
+    Long userId = userDetails.getUser().getUserId();
+    User user = userRepository.getById(userId);
+
+    List<FosterForm> fosterForms = fosterFormRepository.findAllByUser(user);
+    if (fosterForms.size() == 0) {
+      throw new DockingException(ErrorCode.FOSTERFORM_NOT_FOUND);
+    }
+
+    List<FosterPreviewDto> fosterPreviewDtos = new ArrayList<>();
+    for (FosterForm fosterForm : fosterForms) {
+      Post post = fosterForm.getPost();
+      PostPreviewDto postPreviewDto = PostPreviewDto.of(post);
+      FosterPreviewDto fosterPreviewDto = FosterPreviewDto.of(fosterForm, postPreviewDto);
+
+      fosterPreviewDtos.add(fosterPreviewDto);
+    }
+
+    Map<String, Object> data = new HashMap<>();
+    data.put("fosterFormPreviewList", fosterPreviewDtos);
+    return SuccessResult.success(data);
+  }
 }
