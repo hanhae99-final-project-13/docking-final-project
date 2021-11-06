bc39eb27dab88f226356ec0bc11928f93a3cc4de
wldms2494 <wldms2494@gmail.com>
2021-11-06 09:48:57 +0900

    [fix] UpdateRequestDto 추가

---
+++ src/main/java/com/sparta/dockingfinalproject/user/dto/UpdateRequestDto.java
@@ -0,0 +1,21 @@
+package com.sparta.dockingfinalproject.user.dto;
+
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@NoArgsConstructor
+
+@Getter
+@Setter
+public class UpdateRequestDto {
+
+private String nickname;
+private String userImgUrl;
+
+
+
+
+
+
+}
