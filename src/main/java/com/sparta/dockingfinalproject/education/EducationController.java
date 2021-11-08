d2c553d4312f0b80704d4ee11a51a05d5d492074
wldms2494 <wldms2494@gmail.com>
2021-11-09 01:45:17 +0900

    [fix] 에러코드 수정, success메세지 수정

--- src/main/java/com/sparta/dockingfinalproject/education/EducationController.java
+++ src/main/java/com/sparta/dockingfinalproject/education/EducationController.java
@@ -1,6 +1,7 @@
 package com.sparta.dockingfinalproject.education;
 
 import com.sparta.dockingfinalproject.security.UserDetailsImpl;
+import java.util.Map;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -16,8 +17,8 @@ public class EducationController {
   }
 
   @GetMapping("/quiz")
-  public void saveEducation(@AuthenticationPrincipal UserDetailsImpl userDetails, @RequestParam String edu) {
-	educationService.saveEdu(userDetails,edu);
+  public Map<String, Object> saveEducation(@AuthenticationPrincipal UserDetailsImpl userDetails, @RequestParam String edu) {
+	return educationService.saveEdu(userDetails,edu);
   }
 
 }
