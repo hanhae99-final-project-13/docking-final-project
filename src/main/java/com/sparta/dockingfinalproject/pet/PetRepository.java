5476d550ef7cbf820564613784b5c03f7bc5d637
vktm91@nate.com <vktm91@nate.com>
2021-11-12 10:08:30 +0900

    [feat] 입양완료 Post 삭제 스케줄러 추가

--- src/main/java/com/sparta/dockingfinalproject/pet/PetRepository.java
+++ src/main/java/com/sparta/dockingfinalproject/pet/PetRepository.java
@@ -1,29 +1,56 @@
 package com.sparta.dockingfinalproject.pet;
 
-import com.sparta.dockingfinalproject.post.Post;
 import java.time.LocalDateTime;
+import java.util.List;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface PetRepository extends JpaRepository<Pet, Long> {
-  Page<Pet> findAllByCreatedAtBetweenOrderByCreatedAtDesc(LocalDateTime startDt, LocalDateTime endDt, Pageable pageable);
-  Page<Pet> findAllByCreatedAtBetweenAndAddressLikeOrderByCreatedAtDesc(LocalDateTime startDt, LocalDateTime endDt, String address, Pageable pageable);
-  Page<Pet> findAllByCreatedAtBetweenAndOwnerTypeContainingOrderByCreatedAtDesc(LocalDateTime startDt, LocalDateTime endDt, String ownerType, Pageable pageable);
-  Page<Pet> findAllByCreatedAtBetweenAndOwnerTypeContainingAndAddressLikeOrderByCreatedAtDesc(LocalDateTime startDt, LocalDateTime endDt, String ownerType, String address, Pageable pageable);
+
+  Page<Pet> findAllByCreatedAtBetweenOrderByCreatedAtDesc(LocalDateTime startDt,
+      LocalDateTime endDt, Pageable pageable);
+
+  Page<Pet> findAllByCreatedAtBetweenAndAddressLikeOrderByCreatedAtDesc(LocalDateTime startDt,
+      LocalDateTime endDt, String address, Pageable pageable);
+
+  Page<Pet> findAllByCreatedAtBetweenAndOwnerTypeContainingOrderByCreatedAtDesc(
+      LocalDateTime startDt, LocalDateTime endDt, String ownerType, Pageable pageable);
+
+  Page<Pet> findAllByCreatedAtBetweenAndOwnerTypeContainingAndAddressLikeOrderByCreatedAtDesc(
+      LocalDateTime startDt, LocalDateTime endDt, String ownerType, String address,
+      Pageable pageable);
 
   Page<Pet> findAllByOrderByCreatedAtDesc(Pageable pageable);
-  Page<Pet> findAllByAddressLikeOrderByCreatedAtDesc(String address, Pageable pageable);
-  Page<Pet> findAllByOwnerTypeContainingOrderByCreatedAtDesc(String ownerType, Pageable pageable);
-  Page<Pet> findAllByOwnerTypeAndAddressLikeOrderByCreatedAtDesc(String ownerType, String address, Pageable pageable);
 
-  Page<Pet> findAllByCreatedAtBetweenOrderByCreatedAtAsc(LocalDateTime startDt, LocalDateTime endDt, Pageable pageable);
-  Page<Pet> findAllByCreatedAtBetweenAndAddressLikeOrderByCreatedAtAsc(LocalDateTime startDt, LocalDateTime endDt, String address, Pageable pageable);
-  Page<Pet> findAllByCreatedAtBetweenAndOwnerTypeContainingOrderByCreatedAtAsc(LocalDateTime startDt, LocalDateTime endDt, String ownerType, Pageable pageable);
-  Page<Pet> findAllByCreatedAtBetweenAndOwnerTypeContainingAndAddressLikeOrderByCreatedAtAsc(LocalDateTime startDt, LocalDateTime endDt, String ownerType, String address, Pageable pageable);
+  Page<Pet> findAllByAddressLikeOrderByCreatedAtDesc(String address, Pageable pageable);
+
+  Page<Pet> findAllByOwnerTypeContainingOrderByCreatedAtDesc(String ownerType, Pageable pageable);
+
+  Page<Pet> findAllByOwnerTypeAndAddressLikeOrderByCreatedAtDesc(String ownerType, String address,
+      Pageable pageable);
+
+  Page<Pet> findAllByCreatedAtBetweenOrderByCreatedAtAsc(LocalDateTime startDt, LocalDateTime endDt,
+      Pageable pageable);
+
+  Page<Pet> findAllByCreatedAtBetweenAndAddressLikeOrderByCreatedAtAsc(LocalDateTime startDt,
+      LocalDateTime endDt, String address, Pageable pageable);
+
+  Page<Pet> findAllByCreatedAtBetweenAndOwnerTypeContainingOrderByCreatedAtAsc(
+      LocalDateTime startDt, LocalDateTime endDt, String ownerType, Pageable pageable);
+
+  Page<Pet> findAllByCreatedAtBetweenAndOwnerTypeContainingAndAddressLikeOrderByCreatedAtAsc(
+      LocalDateTime startDt, LocalDateTime endDt, String ownerType, String address,
+      Pageable pageable);
 
   Page<Pet> findAllByOrderByCreatedAtAsc(Pageable pageable);
+
   Page<Pet> findAllByAddressLikeOrderByCreatedAtAsc(String address, Pageable pageable);
+
   Page<Pet> findAllByOwnerTypeContainingOrderByCreatedAtAsc(String ownerType, Pageable pageable);
-  Page<Pet> findAllByOwnerTypeContainingAndAddressLikeOrderByCreatedAtAsc(String ownerType, String address, Pageable pageable);
+
+  Page<Pet> findAllByOwnerTypeContainingAndAddressLikeOrderByCreatedAtAsc(String ownerType,
+      String address, Pageable pageable);
+
+  List<Pet> findAllByIsAdoptedOrderByModifiedAtDesc(IsAdopted isAdopted);
 }
---
+++ src/main/java/com/sparta/dockingfinalproject/post/PostDeleteScheduler.java
@@ -0,0 +1,40 @@
+package com.sparta.dockingfinalproject.post;
+
+import com.sparta.dockingfinalproject.pet.IsAdopted;
+import com.sparta.dockingfinalproject.pet.Pet;
+import com.sparta.dockingfinalproject.pet.PetRepository;
+import java.time.LocalDateTime;
+import java.time.temporal.ChronoUnit;
+import java.util.List;
+import org.springframework.scheduling.annotation.Scheduled;
+import org.springframework.stereotype.Component;
+
+@Component
+public class PostDeleteScheduler {
+
+  private final PostRepository postRepository;
+  private final PetRepository petRepository;
+
+  public PostDeleteScheduler(PostRepository postRepository, PetRepository petRepository) {
+    this.postRepository = postRepository;
+    this.petRepository = petRepository;
+  }
+
+  @Scheduled(cron = "0 0 3 * * *")
+  public void adoptedPostDelete() {
+    System.out.println("입양완료 3일 경과 Post 삭제");
+    List<Pet> petList = petRepository.findAllByIsAdoptedOrderByModifiedAtDesc(IsAdopted.adopted);
+
+    for (Pet pet : petList) {
+      LocalDateTime modifiedAt = pet.getModifiedAt();
+      LocalDateTime now = LocalDateTime.now();
+
+      long betweenDays = ChronoUnit.DAYS.between(modifiedAt, now);
+      if (betweenDays >= 3) {
+        postRepository.deleteById(pet.getPost().getPostId());
+        petRepository.deleteById(pet.getPetId());
+      }
+    }
+  }
+
+}
