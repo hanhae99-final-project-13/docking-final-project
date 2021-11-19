a40c702bf1f2351fdc53678ecc7073bd967761ab
vktm91@nate.com <vktm91@nate.com>
2021-11-19 17:29:56 +0900

    [feat] 샘플데이터 추가

--- src/main/java/com/sparta/dockingfinalproject/DockingFinalProjectApplication.java
+++ src/main/java/com/sparta/dockingfinalproject/DockingFinalProjectApplication.java
@@ -3,6 +3,8 @@ package com.sparta.dockingfinalproject;
 import java.util.TimeZone;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.event.ApplicationReadyEvent;
+import org.springframework.context.event.EventListener;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @EnableJpaAuditing
@@ -12,6 +14,11 @@ public class DockingFinalProjectApplication {
     public static void main(String[] args) {
         SpringApplication.run(DockingFinalProjectApplication.class, args);
         TimeZone.setDefault(TimeZone.getTimeZone("Asia/Seoul"));
+    }
+
+    @EventListener(ApplicationReadyEvent.class)
+    public void init() {
+        System.out.println("애플리케이션이 정상 실행 되었습니다.");
     }
 
 }
