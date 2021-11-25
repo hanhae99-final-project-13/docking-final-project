ccb3980f317e17db1de54bf7687c81615d423cdd
zzangoobrother <com1368@naver.com>
2021-11-25 20:27:48 +0900

    [fix] 게시글 댓글 최신순 정렬 수정

--- src/main/java/com/sparta/dockingfinalproject/comment/CommentRepository.java
+++ src/main/java/com/sparta/dockingfinalproject/comment/CommentRepository.java
@@ -8,5 +8,5 @@ import java.util.List;
 
 public interface CommentRepository extends JpaRepository<Comment, Long> {
 
-  List<CommentResponseDto> findAllByPost(Post post);
+  List<CommentResponseDto> findAllByPostOrderByCreatedAtDesc(Post post);
 }
