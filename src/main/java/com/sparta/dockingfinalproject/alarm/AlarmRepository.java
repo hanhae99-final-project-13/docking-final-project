936a265bbc967d74f530a614baad69fb465d9d98
zzangoobrother <com1368@naver.com>
2021-11-27 10:09:27 +0900

    [fix] 댓글 삭제시 알람 미삭제로 인한 오류 수정

--- src/main/java/com/sparta/dockingfinalproject/alarm/AlarmRepository.java
+++ src/main/java/com/sparta/dockingfinalproject/alarm/AlarmRepository.java
@@ -1,6 +1,7 @@
 package com.sparta.dockingfinalproject.alarm;
 
 import com.sparta.dockingfinalproject.alarm.model.Alarm;
+import com.sparta.dockingfinalproject.alarm.model.AlarmType;
 import com.sparta.dockingfinalproject.user.User;
 import java.util.List;
 import org.springframework.data.jpa.repository.JpaRepository;
@@ -9,4 +10,5 @@ public interface AlarmRepository extends JpaRepository<Alarm, Long> {
   List<Alarm> findAllByUserOrderByCreatedAtDesc(User user);
   List<Alarm> findAllByUserAndCheckedTrueOrderByCreatedAtDesc(User user);
   void deleteByUser(User user);
+  void deleteByAlarmTypeAndContentId(AlarmType alarmType, Long contentId);
 }
