0677dd4ad0775a933b523bf95881b0e0f2e7d45e
zzangoobrother <com1368@naver.com>
2021-11-29 21:45:47 +0900

    [fix] 게시글 상세 조회 querydsl 수정

--- src/main/java/com/sparta/dockingfinalproject/post/PostRepositoryCustom.java
+++ src/main/java/com/sparta/dockingfinalproject/post/PostRepositoryCustom.java
@@ -1,11 +1,14 @@
 package com.sparta.dockingfinalproject.post;
 
+import com.sparta.dockingfinalproject.post.dto.PostDetailResponseDto;
 import com.sparta.dockingfinalproject.post.dto.PostSearchRequestDto;
 import com.sparta.dockingfinalproject.post.dto.PostSearchResponseDto;
+import com.sparta.dockingfinalproject.security.UserDetailsImpl;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 
 public interface PostRepositoryCustom {
 
   Page<PostSearchResponseDto> searchPagePost(Pageable pageable, PostSearchRequestDto postSearchRequestDto);
+  PostDetailResponseDto findPostDetail(Long postId, UserDetailsImpl userDetails);
 }
