c3eb96eb9289123644e2841f065cad1992529ac7
zzangoobrother <com1368@naver.com>
2021-11-29 21:48:47 +0900

    [fix] 상세 게시글 댓글 조회 querydsl 수정

--- src/main/java/com/sparta/dockingfinalproject/comment/CommentRepository.java
+++ src/main/java/com/sparta/dockingfinalproject/comment/CommentRepository.java
@@ -1,12 +1,7 @@
 package com.sparta.dockingfinalproject.comment;
 
-import com.sparta.dockingfinalproject.comment.dto.CommentResponseDto;
-import com.sparta.dockingfinalproject.post.Post;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-import java.util.List;
+public interface CommentRepository extends JpaRepository<Comment, Long>, CommentRepositoryCustom {
 
-public interface CommentRepository extends JpaRepository<Comment, Long> {
-
-  List<CommentResponseDto> findAllByPostOrderByCreatedAtDesc(Post post);
 }
