0d5ac66e1fa5e7ce233c84350dae414cc0782dae
wldms2494 <wldms2494@gmail.com>
2021-12-02 00:41:51 +0900

    [fix] 로그인테스트에서 postId 조회 쿼리 dsl로 수정

--- src/test/java/com/sparta/dockingfinalproject/user/UserServiceTest.java
+++ src/test/java/com/sparta/dockingfinalproject/user/UserServiceTest.java
@@ -37,181 +37,187 @@ import static org.mockito.Mockito.when;
 @ExtendWith(MockitoExtension.class)//진짜 Mock 객체를 만들어줌
 class UserServiceTest {
 
-	@InjectMocks
-	private UserService userService;
-	@Mock
-	private UserRepository userRepository;
-	@Mock
-	private PasswordEncoder passwordEncoder;
-	@Mock
-	private JwtTokenProvider jwtTokenProvider;
-	@Mock
-	private EducationRepository educationRepository;
-	@Mock
-	private AlarmRepository alarmRepository;
-	@Mock
-	private RefreshTokenRepository refreshTokenRepository;
-	@Mock
-	private FosterFormRepository fosterFormRepository;
+  @InjectMocks
+  private UserService userService;
+  @Mock
+  private UserRepository userRepository;
+  @Mock
+  private PasswordEncoder passwordEncoder;
+  @Mock
+  private JwtTokenProvider jwtTokenProvider;
+  @Mock
+  private EducationRepository educationRepository;
+  @Mock
+  private AlarmRepository alarmRepository;
+  @Mock
+  private RefreshTokenRepository refreshTokenRepository;
+  @Mock
+  private FosterFormRepository fosterFormRepository;
 
 
-	private User user;
-	private RefreshToken refreshToken;
-	private TokenDto tokenDto;
-	private UserDetailsImpl userDetails;
+  private User user;
+  private RefreshToken refreshToken;
+  private TokenDto tokenDto;
+  private UserDetailsImpl userDetails;
 //
 
 
-	@BeforeEach
-	public void init() {
-		user = new User(1L, "user1", "aaa12345", "홍길동", "sss@naver.com", "", "umgurl", 0L, "");
-		tokenDto = new TokenDto("accessToken", "refreshToken", 18000L);
-		userDetails = new UserDetailsImpl(user);
-	}
+  @BeforeEach
+  public void init() {
+	user = new User(1L, "user1", "aaa12345", "홍길동", "sss@naver.com", "", "umgurl", 0L, "");
+	tokenDto = new TokenDto("accessToken", "refreshToken", 18000L);
+	userDetails = new UserDetailsImpl(user);
+  }
 
-	@Test
-	@DisplayName("회원가입")
-	void registerUser() {
+  @Test
+  @DisplayName("회원가입")
+  void registerUser() {
 
-		SignupRequestDto requestDto = new SignupRequestDto("user1", "aaa12345", "aaa12345", "sss@naver.com",
-				"홍길동", "https://gorokke.shop/image/profileDefaultImg.jpg", 1234, "");
-		User user = new User(requestDto, requestDto.getPassword());
+	SignupRequestDto requestDto = new SignupRequestDto("user1", "aaa12345", "aaa12345", "sss@naver.com",
+		"홍길동", "https://gorokke.shop/image/profileDefaultImg.jpg", 1234, "");
+	User user = new User(requestDto, requestDto.getPassword());
 
-		when(passwordEncoder.encode(requestDto.getPassword())).thenReturn("aa1234");
+	when(passwordEncoder.encode(requestDto.getPassword())).thenReturn("aa1234");
 
-		userService.registerUser(requestDto);
+	userService.registerUser(requestDto);
 
-		assertEquals(user.getUsername(), requestDto.getUsername());
-		assertEquals(user.getPassword(), requestDto.getPassword());
-		assertEquals(user.getUserImgUrl(), requestDto.getUserImgUrl());
+	assertEquals(user.getUsername(), requestDto.getUsername());
+	assertEquals(user.getPassword(), requestDto.getPassword());
+	assertEquals(user.getUserImgUrl(), requestDto.getUserImgUrl());
 
-	}
+  }
 
-	@Test
-	@DisplayName("회원가입 유저네임이 중복된경우")
-	void registerUser1() {
+  @Test
+  @DisplayName("회원가입 유저네임이 중복된경우")
+  void registerUser1() {
 
-		SignupRequestDto requestDto = new SignupRequestDto("user1", "aaa12345", "aaa12345", "sss@naver.com",
-				"홍길동", "https://gorokke.shop/image/profileDefaultImg.jpg", 1234, "");
-		User user = new User(requestDto, requestDto.getPassword());
+	SignupRequestDto requestDto = new SignupRequestDto("user1", "aaa12345", "aaa12345", "sss@naver.com",
+		"홍길동", "https://gorokke.shop/image/profileDefaultImg.jpg", 1234, "");
+	User user = new User(requestDto, requestDto.getPassword());
 
-		when(userRepository.findByUsername("user1"))
-				.thenReturn(Optional.of(user));
-
-
-		DockingException exception = assertThrows(DockingException.class, () -> {
-			userService.registerUser(requestDto);
-		});
-
-		assertEquals(exception.getErrorCode(), ErrorCode.USERNAME_DUPLICATE);
-	}
-
-	@Test
-	@DisplayName("회원가입 닉네임이 중복된경우")
-	void registerUser2() {
-
-		SignupRequestDto requestDto = new SignupRequestDto("user1", "aaa12345", "aaa12345", "sss@naver.com",
-				"홍길동", "https://gorokke.shop/image/profileDefaultImg.jpg", 1234, "");
-		User user = new User(requestDto, requestDto.getPassword());
-
-		when(userRepository.findByUsername("user1"))
-				.thenReturn(Optional.of(user));
+	when(userRepository.findByUsername("user1"))
+		.thenReturn(Optional.of(user));
 
 
-		DockingException exception = assertThrows(DockingException.class, () -> {
-			userService.registerUser(requestDto);
-		});
+	DockingException exception = assertThrows(DockingException.class, () -> {
+	  userService.registerUser(requestDto);
+	});
 
-		assertEquals(exception.getErrorCode(), ErrorCode.USERNAME_DUPLICATE);
-	}
+	assertEquals(exception.getErrorCode(), ErrorCode.USERNAME_DUPLICATE);
+  }
 
+  @Test
+  @DisplayName("회원가입 닉네임이 중복된경우")
+  void registerUser2() {
 
-	@Test
-	@DisplayName("로그인")
-	void login() {
-		UserRequestDto requestDto = new UserRequestDto("user1", "aaa12345");
+	SignupRequestDto requestDto = new SignupRequestDto("user1", "aaa12345", "aaa12345", "sss@naver.com",
+		"홍길동", "https://gorokke.shop/image/profileDefaultImg.jpg", 1234, "");
+	User user = new User(requestDto, requestDto.getPassword());
 
-		refreshToken = RefreshToken.builder()
-				.key(requestDto.getUsername())
-				.value("refreshToken")
-				.build();
-
-		Education education = new Education(user);
+	when(userRepository.findByUsername("user1"))
+		.thenReturn(Optional.of(user));
 
 
-		FosterFormRequestDto fosterFormRequestDto = new FosterFormRequestDto("", 1L, "m", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "","");
+	DockingException exception = assertThrows(DockingException.class, () -> {
+	  userService.registerUser(requestDto);
+	});
 
-		Post post = new Post(100L, 0L, null, user, new ArrayList<>(), new ArrayList<>(), new ArrayList<>());
-		Post post1 = new Post(101L, 0L, null, user, new ArrayList<>(), new ArrayList<>(), new ArrayList<>());
-		Post post2 = new Post(102L, 0L, null, user, new ArrayList<>(), new ArrayList<>(), new ArrayList<>());
-		FosterForm fosterForm = new FosterForm(post, fosterFormRequestDto, null, null);
-		FosterForm fosterForm1 = new FosterForm(post1, fosterFormRequestDto, null, null);
-		FosterForm fosterForm2 = new FosterForm(post2, fosterFormRequestDto, null, null);
-
-		List<FosterForm> fosterFormList = new ArrayList<>();
-		fosterFormList.add(fosterForm);
-		fosterFormList.add(fosterForm1);
-		fosterFormList.add(fosterForm2);
+	assertEquals(exception.getErrorCode(), ErrorCode.USERNAME_DUPLICATE);
+  }
 
 
-		when(passwordEncoder.matches(requestDto.getPassword(), user.getPassword())).thenReturn(true);
-		when(userRepository.findByUsername(requestDto.getUsername())).thenReturn(Optional.of(user));
-		when(jwtTokenProvider.createToken(requestDto.getUsername(),
-				requestDto.getUsername())).thenReturn(tokenDto);
-		when(educationRepository.findByUser(user)).thenReturn(Optional.of(education));
+  @Test
+  @DisplayName("로그인")
+  void login() {
+	UserRequestDto requestDto = new UserRequestDto("user1", "aaa12345");
 
-		when(fosterFormRepository.findAllByUser(user)).thenReturn(fosterFormList);
+	refreshToken = RefreshToken.builder()
+		.key(requestDto.getUsername())
+		.value("refreshToken")
+		.build();
 
-		userService.login(requestDto);
-
-		assertEquals(requestDto.getUsername(), user.getUsername());
-		assertEquals(requestDto.getPassword(), user.getPassword());
-	}
-
-	@Test
-	@DisplayName("로그인 실패 - 비밀번호가 틀릴경우")
-	void login_pw() {
-		UserRequestDto requestDto = new UserRequestDto("user1", "bb1234");
-
-		when(passwordEncoder.matches(requestDto.getPassword(), user.getPassword())).thenReturn(false);
-		when(userRepository.findByUsername(requestDto.getUsername())).thenReturn(Optional.of(user));
-
-		DockingException exception = assertThrows(DockingException.class, () -> {
-			userService.login(requestDto);
-		});
-
-		assertEquals(exception.getErrorCode(), ErrorCode.PASSWORD_NOT_FOUND);
-	}
-
-	@Test
-	@DisplayName("로그인실패 - 아이디가 틀린경우")
-	void login_username() {
-		UserRequestDto requestDto = new UserRequestDto("user2", "aa1234");
-		when(userRepository.findByUsername(requestDto.getUsername()))
-				.thenThrow(new DockingException(ErrorCode.USERNAME_NOT_FOUND));
-
-		DockingException exception = assertThrows(DockingException.class, () -> {
-			userService.login(requestDto);
-		});
-		assertEquals(exception.getErrorCode(), ErrorCode.USERNAME_NOT_FOUND);
-
-	}
+	Education education = new Education(user);
 
 
-	@Test
-	@DisplayName("회원 정보 수정")
-	void updateUser() {
-		UpdateRequestDto requestDto = new UpdateRequestDto("지은짱", "imgurl2");
-		when(userRepository.findById(userDetails.getUser().getUserId())).thenReturn(Optional.of(user));
+	FosterFormRequestDto fosterFormRequestDto = new FosterFormRequestDto("", 1L, "m", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "","");
 
-		userService.updateUser(userDetails, requestDto);
+	Post post = new Post(100L, 0L, null, user, new ArrayList<>(), new ArrayList<>(), new ArrayList<>());
+	Post post1 = new Post(101L, 0L, null, user, new ArrayList<>(), new ArrayList<>(), new ArrayList<>());
+	Post post2 = new Post(102L, 0L, null, user, new ArrayList<>(), new ArrayList<>(), new ArrayList<>());
+	FosterForm fosterForm = new FosterForm(post, fosterFormRequestDto, null, null);
+	FosterForm fosterForm1 = new FosterForm(post1, fosterFormRequestDto, null, null);
+	FosterForm fosterForm2 = new FosterForm(post2, fosterFormRequestDto, null, null);
 
-		assertEquals(userDetails.getUser().getNickname(), "지은짱");
-		assertEquals(userDetails.getUser().getUserImgUrl(), "imgurl2");
-	}
+	List<FosterForm> fosterFormList = new ArrayList<>();
+	fosterFormList.add(fosterForm);
+	fosterFormList.add(fosterForm1);
+	fosterFormList.add(fosterForm2);
+
+	List<Long> postIdList = new ArrayList<>();
+	postIdList.add(fosterForm.getPost().getPostId());
+	postIdList.add(fosterForm1.getPost().getPostId());
 
 
-	//
+
+	when(passwordEncoder.matches(requestDto.getPassword(), user.getPassword())).thenReturn(true);
+	when(userRepository.findByUsername(requestDto.getUsername())).thenReturn(Optional.of(user));
+	when(jwtTokenProvider.createToken(requestDto.getUsername(),
+		requestDto.getUsername())).thenReturn(tokenDto);
+	when(educationRepository.findByUser(user)).thenReturn(Optional.of(education));
+
+//		when(fosterFormRepository.findAllByUser(user)).thenReturn(fosterFormList);
+
+	when(userRepository.getPostIdFromFosterForm(user)).thenReturn(postIdList);
+	userService.login(requestDto);
+
+	assertEquals(requestDto.getUsername(), user.getUsername());
+	assertEquals(requestDto.getPassword(), user.getPassword());
+  }
+
+  @Test
+  @DisplayName("로그인 실패 - 비밀번호가 틀릴경우")
+  void login_pw() {
+	UserRequestDto requestDto = new UserRequestDto("user1", "bb1234");
+
+	when(passwordEncoder.matches(requestDto.getPassword(), user.getPassword())).thenReturn(false);
+	when(userRepository.findByUsername(requestDto.getUsername())).thenReturn(Optional.of(user));
+
+	DockingException exception = assertThrows(DockingException.class, () -> {
+	  userService.login(requestDto);
+	});
+
+	assertEquals(exception.getErrorCode(), ErrorCode.PASSWORD_NOT_FOUND);
+  }
+
+  @Test
+  @DisplayName("로그인실패 - 아이디가 틀린경우")
+  void login_username() {
+	UserRequestDto requestDto = new UserRequestDto("user2", "aa1234");
+	when(userRepository.findByUsername(requestDto.getUsername()))
+		.thenThrow(new DockingException(ErrorCode.USERNAME_NOT_FOUND));
+
+	DockingException exception = assertThrows(DockingException.class, () -> {
+	  userService.login(requestDto);
+	});
+	assertEquals(exception.getErrorCode(), ErrorCode.USERNAME_NOT_FOUND);
+
+  }
+
+
+  @Test
+  @DisplayName("회원 정보 수정")
+  void updateUser() {
+	UpdateRequestDto requestDto = new UpdateRequestDto("지은짱", "imgurl2");
+	when(userRepository.findById(userDetails.getUser().getUserId())).thenReturn(Optional.of(user));
+
+	userService.updateUser(userDetails, requestDto);
+
+	assertEquals(userDetails.getUser().getNickname(), "지은짱");
+	assertEquals(userDetails.getUser().getUserImgUrl(), "imgurl2");
+  }
+
+
+  //
 //    @Test
 //    @Disabled
 //    @DisplayName("로그인체크")
